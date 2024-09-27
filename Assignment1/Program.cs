26c6481e6e8e2c32912dc2a43722769b863ca6ad
Teddylikeabear <nyatshiisana@gmail.com>
2024-09-27 08:25:56 +0200

    Add project files.

---
+++ Assignment1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Assignment1
+{
+    public class BeerSong
+    {
+        public void SingSong()
+        {
+            int bottles = 99;
+
+            do
+            {
+                if (bottles > 1)
+                {
+                    Console.WriteLine($"{bottles} bottles of beer on the wall, {bottles} bottles of beer.");
+                    Console.WriteLine($"Take one down and pass it around, {bottles - 1} bottles of beer on the wall.");
+                }
+                else
+                {
+                    Console.WriteLine("1 bottle of beer on the wall, 1 bottle of beer.");
+                    Console.WriteLine("Take one down and pass it around, no more bottles of beer on the wall.");
+                }
+
+                Console.WriteLine();
+                bottles--;
+
+            } while (bottles > 0);
+
+            Console.WriteLine("No more bottles of beer on the wall, no more bottles of beer.");
+           
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            BeerSong song = new BeerSong();
+            song.SingSong();
+        }
+    }
+
+
+}
